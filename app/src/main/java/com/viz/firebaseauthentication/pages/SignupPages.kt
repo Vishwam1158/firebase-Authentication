8da5da19754c9547394e275c6b19133643fe7403
Vishwam1158 <vishwam1158@gmail.com>
2024-06-15 22:00:55 +0530

    add signup Page

---
+++ app/src/main/java/com/viz/firebaseauthentication/pages/SignupPages.kt
@@ -0,0 +1,75 @@
+package com.viz.firebaseauthentication.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.viz.firebaseauthentication.AuthViewModel
+
+@Composable
+fun SignupPage(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    authViewModel: AuthViewModel
+) {
+    var email by remember { mutableStateOf("") }
+    var pass by remember { mutableStateOf("") }
+
+    val authState = authViewModel.authState.observeAsState()
+
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Signup Page", fontSize = 32.sp)
+        Spacer(modifier = modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text(text = "Email") }
+        )
+        Spacer(modifier = modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = pass,
+            onValueChange = { pass = it },
+            label = { Text(text = "Password") }
+        )
+        Spacer(modifier = modifier.height(16.dp))
+
+        Button(
+            onClick = { authViewModel.signup(email, pass) }
+        ) {
+            Text(text = "Create Account")
+        }
+        Spacer(modifier = modifier.height(8.dp))
+
+        TextButton(
+            onClick = { navController.navigate("login") }
+        ) {
+            Text(text = "Already have account, Sign Up")
+        }
+
+
+
+    }
+}
