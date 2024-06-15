0c938bf5baf011c623c481c9e5b64af12dfbe6bf
Vishwam1158 <vishwam1158@gmail.com>
2024-06-15 22:13:40 +0530

    add signup details

--- app/src/main/java/com/viz/firebaseauthentication/pages/SignupPages.kt
+++ app/src/main/java/com/viz/firebaseauthentication/pages/SignupPages.kt
@@ -1,5 +1,6 @@
 package com.viz.firebaseauthentication.pages
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
@@ -17,9 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.viz.firebaseauthentication.AuthState
 import com.viz.firebaseauthentication.AuthViewModel
 
 @Composable
@@ -32,7 +36,14 @@ fun SignupPage(
     var pass by remember { mutableStateOf("") }
 
     val authState = authViewModel.authState.observeAsState()
-
+    val context = LocalContext.current
+    LaunchedEffect(authState.value) {
+        when(authState.value) {
+            is AuthState.Authenticated -> navController.navigate("home")
+            is AuthState.Error -> Toast.makeText(context, (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT).show()
+            else -> {}
+        }
+    }
 
     Column(
         modifier = modifier.fillMaxSize(),
